e4e61ae58852ef6c3b7753350207f2f9c8c7a582
Tony133 <a.tripodi133@gmail.com>
2024-06-23 22:30:34 +0200

    fix: marked api public

--- lib/decorators/api-response.decorator.ts
+++ lib/decorators/api-response.decorator.ts
@@ -84,66 +84,103 @@ export function ApiResponse(
   };
 }
 
+/**
+ * @publicApi
+ */
 export const ApiOkResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.OK
   });
 
+/**
+ * @publicApi
+ */
 export const ApiCreatedResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.CREATED
   });
 
+/**
+ * @publicApi
+ */
 export const ApiAcceptedResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.ACCEPTED
   });
 
+/**
+ * @publicApi
+ */
 export const ApiNoContentResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.NO_CONTENT
   });
 
+/**
+ * @publicApi
+ */
 export const ApiMovedPermanentlyResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.MOVED_PERMANENTLY
   });
 
+/**
+ * @publicApi
+ */
 export const ApiFoundResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.FOUND
   });
 
+/**
+ * @publicApi
+ */
 export const ApiBadRequestResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.BAD_REQUEST
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiUnauthorizedResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.UNAUTHORIZED
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiTooManyRequestsResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.TOO_MANY_REQUESTS
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiNotFoundResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.NOT_FOUND
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiInternalServerErrorResponse = (
   options: ApiResponseOptions = {}
 ) =>
@@ -152,54 +189,90 @@ export const ApiInternalServerErrorResponse = (
     status: HttpStatus.INTERNAL_SERVER_ERROR
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiBadGatewayResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.BAD_GATEWAY
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiConflictResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.CONFLICT
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiForbiddenResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.FORBIDDEN
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiGatewayTimeoutResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.GATEWAY_TIMEOUT
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiGoneResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.GONE
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiMethodNotAllowedResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.METHOD_NOT_ALLOWED
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiNotAcceptableResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.NOT_ACCEPTABLE
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiNotImplementedResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.NOT_IMPLEMENTED
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiPreconditionFailedResponse = (
   options: ApiResponseOptions = {}
 ) =>
@@ -208,18 +281,30 @@ export const ApiPreconditionFailedResponse = (
     status: HttpStatus.PRECONDITION_FAILED
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiPayloadTooLargeResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.PAYLOAD_TOO_LARGE
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiRequestTimeoutResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
     status: HttpStatus.REQUEST_TIMEOUT
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiServiceUnavailableResponse = (
   options: ApiResponseOptions = {}
 ) =>
@@ -228,6 +313,10 @@ export const ApiServiceUnavailableResponse = (
     status: HttpStatus.SERVICE_UNAVAILABLE
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiUnprocessableEntityResponse = (
   options: ApiResponseOptions = {}
 ) =>
@@ -236,6 +325,10 @@ export const ApiUnprocessableEntityResponse = (
     status: HttpStatus.UNPROCESSABLE_ENTITY
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiUnsupportedMediaTypeResponse = (
   options: ApiResponseOptions = {}
 ) =>
@@ -244,6 +337,10 @@ export const ApiUnsupportedMediaTypeResponse = (
     status: HttpStatus.UNSUPPORTED_MEDIA_TYPE
   });
 
+
+/**
+ * @publicApi
+ */
 export const ApiDefaultResponse = (options: ApiResponseOptions = {}) =>
   ApiResponse({
     ...options,
